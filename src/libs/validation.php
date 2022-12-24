e788ded7cd55b7e2b7d53481a5aeadec4f66944b
Alexander <aircorridor@proton.me>
2022-12-24 21:13:25 +0200

    Added validation library

--- src/libs/validation.php
+++ src/libs/validation.php
@@ -17,12 +17,18 @@
         'unique' => 'The %s already exists',
     ];
 
-    function validate(array $data, array $fields):array{
-        // split a str by a separator
-        // trim each item in the result array and return it
+    function validate(array $data, array $fields, array $messages = []):array{
+        $errors = [];
+
+        // Split the array by a separator, trim each element
+        // and return the array
         $split = fn($str, $separator) => array_map('trim', explode($separator, $str));
 
-        $errors = [];
+        // get the message rules
+        $rule_messages = array_filter($messages, fn($message) =>  is_string($message));
+
+        // overwrite the default message
+        $validation_errors = array_merge(DEFAULT_VALIDATION_ERRORS, $messages);
 
         foreach ($fields as $field => $option){
             // get the rules of the field
@@ -47,11 +53,86 @@
                 if(is_callable($fn)){
                     $pass = $fn($data, $field, ...$params);
                     if (!$pass){
-                        $errors[$field] = sprintf(DEFAULT_VALIDATION_ERRORS[$rule_name], $field, ...$params);
+                        // get the error message for a specific field and rule if exists
+                        // otherwise get the error message from the $validation_errors
+                        $errors[$field] = sprintf(
+                            $messages[$field][$rule_name] ?? $validation_errors[$rule_name],
+                            $field,
+                            ...$params
+                        );
                     }
                 }
             }
         }
         return $errors;
     }
+
+    function is_required(array $data, string $field): bool{
+        return isset($data[$field]) && trim($data[$field]) !== '';
+    }
+
+    function is_email(array $data, string $field): bool{
+        if (empty($data[$field])) {
+            return true;
+        }
+
+        return filter_var($data[$field], FILTER_VALIDATE_EMAIL);
+    }
+
+    function is_min(array $data, string $field, int $min): bool{
+        if (!isset($data[$field])) {
+            return true;
+        }
+
+        return mb_strlen($data[$field]) >= $min;
+    }
+
+    function is_max(array $data, string $field, int $max): bool{
+        if (!isset($data[$field])) {
+            return true;
+        }
+
+        return mb_strlen($data[$field]) <= $max;
+    }
+
+    function is_between(array $data, string $field, int $min, int $max): bool{
+        if (!isset($data[$field])) {
+            return true;
+        }
+
+        $len = mb_strlen($data[$field]);
+        return $len >= $min && $len <= $max;
+    }
+
+    function is_same(array $data, string $field, string $other): bool{
+        if (isset($data[$field], $data[$other])) {
+            return $data[$field] === $data[$other];
+        }
+
+        if (!isset($data[$field]) && !isset($data[$other])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    function is_alphanumeric(array $data, string $field): bool
+    {
+        if (!isset($data[$field])) {
+            return true;
+        }
+
+        return ctype_alnum($data[$field]);
+    }
+
+    function is_secure(array $data, string $field): bool
+    {
+        if (!isset($data[$field])) {
+            return false;
+        }
+
+        $pattern = "#.*^(?=.{8,64})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$#";
+        return preg_match($pattern, $data[$field]);
+    }
+
 ?>
