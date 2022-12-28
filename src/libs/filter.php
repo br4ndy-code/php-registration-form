498ad04968c6d93a247d9ae6beead591fa676563
Alexander <aircorridor@proton.me>
2022-12-28 14:07:17 +0200

    Comment the filter function

--- src/libs/filter.php
+++ src/libs/filter.php
@@ -1,28 +1,24 @@
 <?php
 
-/**
- * Sanitize and validate data
- * @param array $data
- * @param array $fields
- * @param array $messages
- * @return array
- */
-function filter(array $data, array $fields, array $messages = []): array
-{
-    $sanitization = [];
-    $validation = [];
+    function filter(array $data, array $fields, array $messages = []): array{
+        $sanitization = [];
+        $validation = [];
 
-    // extract sanitization & validation rules
-    foreach ($fields as $field => $rules) {
-        if (strpos($rules, '|')) {
-            [$sanitization[$field], $validation[$field]] = explode('|', $rules, 2);
-        } else {
-            $sanitization[$field] = $rules;
+        // extract sanitization & validation rules
+        foreach ($fields as $field => $rules) {
+            if (strpos($rules, '|')) {
+                [$sanitization[$field], $validation[$field]] = explode('|', $rules, 2);
+                /*
+                $sanitisation will be 'name' => 'string',
+                $validation will be 'name' => 'required | max: 255',
+                */
+            } else {
+                $sanitization[$field] = $rules;
+            }
         }
+
+        $inputs = sanitize($data, $sanitization);
+        $errors = validate($inputs, $validation, $messages);
+
+        return [$inputs, $errors];
     }
-
-    $inputs = sanitize($data, $sanitization);
-    $errors = validate($inputs, $validation, $messages);
-
-    return [$inputs, $errors];
-}
